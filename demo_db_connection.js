5f10464a12571eaa42ac33c4a37a3f0e063a7e47
Nifilia <anderokore@gmail.com>
2022-02-07 11:25:37 +0200

    insert all

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -10,8 +10,25 @@ var con = mysql.createConnection({
 con.connect(function(err){
 	if(err) throw err;
 	console.log("Connected!");
-	con.query("SELECT * FROM customers ORDER BY name", function(err, result){
+	var sql = "INSERT INTO customers (name, address) VALUES ?";
+	var values = [
+    ['John', 'Highway 71'],
+    ['Peter', 'Lowstreet 4'],
+    ['Amy', 'Apple st 652'],
+    ['Hannah', 'Mountain 21'],
+    ['Michael', 'Valley 345'],
+    ['Sandy', 'Ocean blvd 2'],
+    ['Betty', 'Green Grass 1'],
+    ['Richard', 'Sky st 331'],
+    ['Susan', 'One way 98'],
+    ['Vicky', 'Yellow Garden 2'],
+    ['Ben', 'Park Lane 38'],
+    ['William', 'Central st 954'],
+    ['Chuck', 'Main Road 989'],
+    ['Viola', 'Sideway 1633']
+	];
+	con.query(sql, [values], function(err, result){
 		if(err) throw err;
-		console.log(result);
+		console.log("Number of records inserted: " + result.affectedRows);
 	})
 })
